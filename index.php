6fc9a50dfa09d3f6860308fbdaa3b87b6f0ef213
Superchick-87 <nicolas.peyrebrune@gmail.com>
2023-08-11 17:16:35 +0200

    maj

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/styles.css" rel="stylesheet">
+    <title>Document</title>
+</head>
+
+<body>
+    <p class="containt intro">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, illum voluptatum voluptates rerum minus ad soluta totam corrupti ex debitis, fuga minima qui consequatur suscipit dolorem iste nihil omnis in?</p>
+    <div id="map">
+        <?php
+        include('images/minimap.svg');
+        ?>
+    </div>
+    <div id="L1" class="st5 containt">
+        <h2>Bastide</h2>
+        <p class="containt">La place André-Meunier comme la plaine des sports offrent des espaces de fraîcheur dans un quartier très minéralisé et largement couvert par les voies ferrées. La gare Saint-Jean, pourtant renovée récemment, constitue un important îlot de chaleur. La végétation offre une couverture bénéfique au sud du quartier, à la frontière de Bègles. </p>
+        <img src="images/visu_01.jpg" alt="">
+    </div>
+    <div id="L4" class="st5 containt">
+        <h2>Bordeaux - Sud</h2>
+        <p class="containt">Le secteur historique, quasi totalement dénué de végétation, est un immense îlot de chaleur. Au nord du quartier, la végétation de part et d'autre des Quinconces offre visiblement un espace de fraîcheur.</p>
+        <img src="images/visu_04.jpg" alt="">
+    </div>
+</body>
+<script src="js/svg.js"></script>
+
+</html>
